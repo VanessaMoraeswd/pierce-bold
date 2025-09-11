6e6bab69fdb6a9a6ac95b8856897d9657e5f1094
Vanessa <vanessamoraeswd@gmail.com>
2025-09-11 16:36:21 -0300

    Create multiple pages using react-router

--- piercebold/src/App.jsx
+++ piercebold/src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
-
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import './App.css'
 
 export function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='checkout' element={<CheckoutPage />} />
+      <Route path='checkout' element={<div>Test checkout page</div>} />
+      <Route path='orders' element={<OrdersPage />} />
+    </Routes>
   )
 }
---
+++ piercebold/src/pages/OrdersPage.jsx
@@ -0,0 +1,7 @@
+export function OrdersPage() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
