06e2f589cea22c156e6355137e414c0944fefaef
Vanessa <vanessamoraeswd@gmail.com>
2025-09-20 00:12:01 -0300

    Move Checkout and CheckoutHeader to pages/Checkout

--- piercebold/src/App.jsx
+++ piercebold/src/App.jsx
@@ -1,16 +1,17 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/Checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
 import { TrackingPage } from './pages/TrackingPage'
 import './App.css'
+import './index.css'
 
 export function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path='checkout' element={<CheckoutPage />} />
-      <Route path='checkout' element={<div>Test checkout page</div>} />
+      {/* <Route path='checkout' element={<div>Test checkout page</div>} /> */}
       <Route path='orders' element={<OrdersPage />} />
       <Route path='tracking' element={<TrackingPage />} />
     </Routes>
