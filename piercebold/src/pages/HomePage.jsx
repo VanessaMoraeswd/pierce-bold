ec207f3e750bd39de536bcb3703d3b36ab63c052
Vanessa <vanessamoraeswd@gmail.com>
2025-09-25 14:54:49 -0300

    Use axios to fetch the data

--- piercebold/src/pages/HomePage.jsx
+++ piercebold/src/pages/HomePage.jsx
@@ -1,19 +1,19 @@
+import axios from 'axios'
 import { Header } from '../components/Header'
 import { products } from '../../../starting-code/data/products'
 import './HomePage.css'
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
-  .then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)
-  })
+  axios
+    .get('http://localhost:3000/api/products')
+    .then((response) => {
+      console.log(response.data)
+    })
 
   return (
     <>
       <title>Pierce Bold</title>
- 
+
       <Header />
 
       <div className='home-page'>
