c8bad4595c48d5f7f34c9d59a4fd31d80ab01293
Vanessa Moraes <vanessamoraeswd@gmail.com>
2026-01-02 17:50:41 -0300

    Delete kart item feature

--- piercebold/src/pages/Checkout/OrderSummary.jsx
+++ piercebold/src/pages/Checkout/OrderSummary.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import dayjs from 'dayjs'
 import { formatMoney } from '../../utils/money'
 import { DeliveryOptions } from './DeliveryOptions'
@@ -13,6 +14,12 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
                 return deliveryOption.id === cartItem.deliveryOptionId
               }
             )
+
+            const deleteCartItem = async () => {
+              await axios.delete(`/api/cart-items/${cartItem.productId}`)
+              await loadCart()
+            }
+
             return (
               <div key={cartItem.productId} className='cart-item-container'>
                 <div className='delivery-date'>
@@ -40,7 +47,10 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
                       <span className='update-quantity-link link-primary'>
                         Update
                       </span>
-                      <span className='delete-quantity-link link-primary'>
+                      <span
+                        className='delete-quantity-link link-primary'
+                        onClick={deleteCartItem}
+                      >
                         Delete
                       </span>
                     </div>
